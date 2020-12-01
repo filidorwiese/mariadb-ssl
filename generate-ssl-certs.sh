4388b408c75f12b2c7b0bbef6096e1f6f0eda939
Filidor Wiese <fili@fili.nl>
2020-12-01 15:26:36 +0100

    Removes days flag from openssl req command, not needed

--- generate-ssl-certs.sh
+++ generate-ssl-certs.sh
@@ -9,8 +9,8 @@ chmod 700 /etc/mysql/ssl
 cd /etc/mysql/ssl
 
 openssl genrsa 2048 > ca-key.pem
-openssl req -new -x509 -nodes -days 365000 -key ca-key.pem -out ca-cert.pem -subj "$OPENSSL_SUBJECT"
-openssl req -newkey rsa:2048 -days 365000 -nodes -keyout server-key.pem -out server-req.pem -subj "$OPENSSL_SUBJECT"
+openssl req -new -x509 -nodes -key ca-key.pem -out ca-cert.pem -subj "$OPENSSL_SUBJECT"
+openssl req -newkey rsa:2048 -nodes -keyout server-key.pem -out server-req.pem -subj "$OPENSSL_SUBJECT"
 openssl rsa -in server-key.pem -out server-key.pem
 openssl x509 -req -in server-req.pem -days 365000 -CA ca-cert.pem -CAkey ca-key.pem -set_serial 01 -out server-cert.pem
 chown -R mysql:root /etc/mysql/ssl
