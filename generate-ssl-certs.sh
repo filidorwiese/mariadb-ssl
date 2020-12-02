85d5a961f084b8bdc22ab42b5127d7458f81d2c0
Filidor Wiese <fili@fili.nl>
2020-12-02 12:12:23 +0100

    Root-ca common-name needs to differ from the server-cert common-name - https://dev.mysql.com/doc/refman/8.0/en/creating-ssl-files-using-openssl.html

--- generate-ssl-certs.sh
+++ generate-ssl-certs.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -e
 
 : "${OPENSSL_SUBJECT:="/C=NL/ST=State/L=City/O=Organization/OU=Department/CN=server.fqdn"}"
+: "${OPENSSL_CA_SUBJECT:="/C=NL/ST=State/L=City/O=Organization/OU=Department/CN=ca.fqdn"}"
 
 if [ ! -f /etc/mysql/mariadb.conf.d/55-server-ssl.cnf ]; then
 
@@ -9,8 +10,8 @@ chmod 700 /etc/mysql/ssl
 cd /etc/mysql/ssl
 
 openssl genrsa 2048 > ca-key.pem
-openssl req -new -x509 -nodes -key ca-key.pem -out ca-cert.pem -subj "$OPENSSL_SUBJECT"
-openssl req -newkey rsa:2048 -nodes -keyout server-key.pem -out server-req.pem -subj "$OPENSSL_SUBJECT"
+openssl req -new -x509 -nodes -key ca-key.pem -out ca-cert.pem -subj "${OPENSSL_CA_SUBJECT}"
+openssl req -newkey rsa:2048 -nodes -keyout server-key.pem -out server-req.pem -subj "${OPENSSL_SUBJECT}"
 openssl rsa -in server-key.pem -out server-key.pem
 openssl x509 -req -in server-req.pem -days 365000 -CA ca-cert.pem -CAkey ca-key.pem -set_serial 01 -out server-cert.pem
 chown -R mysql:root /etc/mysql/ssl
